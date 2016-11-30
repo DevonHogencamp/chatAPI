ee2ca6e672067108eb1cf81b97795ef200b74a59
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-11-30 13:51:36 -0700

    created api user and msg

---
+++ api/controllers/MsgController.js
@@ -0,0 +1,11 @@
+/**
+ * MsgController
+ *
+ * @description :: Server-side logic for managing msgs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
